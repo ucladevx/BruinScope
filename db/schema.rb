781ffd755a1898dbc9e60c3447a9838ac3c5af43
jcherianucla <jcherian@ucla.edu>
2017-05-01 21:42:07 -0700

    Company added into schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170425225754) do
+ActiveRecord::Schema.define(version: 20170502032136) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "companies", force: :cascade do |t|
+    t.string   "name"
+    t.text     "location"
+    t.string   "link"
+    t.text     "description"
+    t.string   "image"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
