af0fad9e9f062290cd8d461c64723bb916c6c7cb
jcherianucla <jcherian@ucla.edu>
2017-05-10 21:38:33 -0700

    Added company controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :posts do
     get 'upvote' => 'posts#upvote', as: :upvote
   end
+  resources :companies
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 	# Root path
