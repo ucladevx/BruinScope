f35e5e674bb7b49d456a5403ff22182d917926c9
Shivam Thapar <shivam.thapar1@gmail.com>
2017-05-12 19:29:16 -0700

    Update postcard view, add partial for not logged in users
    * add default Post seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,3 +19,46 @@ company_list = [
 company_list.each do |name, location|
 	Company.create(name: name, location: location)
 end
+
+
+
+if User.count > 0
+  user = User.first
+
+  posts = [
+    {
+      title: "Great experience but took longer than expected",
+      experience: "Phone screening with human resources, as a first assessment. Then first stage of phone coding interview using codepad focused on algorithmic question…",
+      question: "Given the alphabet encoded as numbers (e.g., a=1, b=2, ..., z=26), and a sequence of numbers (e.g., \"23413259802\"), how many strings can be generated.",
+      resources: "CS 118",
+      company: "Facebook",
+      position: "Software Engineering Internship",
+      created_at: "13-12-2017".to_time    
+    },
+    {
+      title: "Very Technical Interview",
+      experience: "As most others have noted, the interview process is a 30-minute phone screener with a recruiter, then two back-to-back 45-minute video…",
+      question: "Design a way for people with similar interests/hobbies to connect.",
+      resources: "CS 131",
+      company: "Uber",
+      position: "Product Management Internship"
+    },
+    {
+      title: "Pleasant experience. No complaints.",
+      experience: "First Round: call with HR to discuss background and interest in fb. Then several multiple choice questions on Android. If you answer enough questions...",
+      question: "Given inorder and postorder traversals, reconstruct a binary tree.",
+      resources: "CS 35L",
+      company: "Google",
+      position: "Software Engineering Internship"
+    },
+  ]
+
+  posts.each do |post|
+    company = Company.where(name: post[:company]).first
+    new_post = Post.create(title: post[:title], experience: post[:experience], question: post[:question], resources: post[:resources], position: post[:position]) do |p|
+      p.user = user
+      p.company = company
+    end
+    new_post.save
+  end
+end
