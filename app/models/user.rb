946504b3b30ccb991eae145a3c685dbf855a1d2b
jcherianucla <jcherian@ucla.edu>
2017-05-20 19:53:56 -0700

    Adding profile picture options to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,16 @@ class User < ApplicationRecord
   # acts_as_votable: Users "act as voter" to provide some reserve functionality.
   acts_as_voter
 
+  # Profile image uploader 
+  mount_uploader :avatar, AvatarUploader
+
+  # Validate image integrity
+  validates_integrity_of :avatar
+  # Validate image processing
+  validates_processing_of :avatar
+  # Validate image size
+  validates :avatar_size_validation
+
   def self.from_omniauth(access_token)
     data = access_token.info
     user = User.where(:email => data["email"]).first
@@ -27,5 +37,10 @@ class User < ApplicationRecord
   def full_name
     return "#{self.first_name} #{self.last_name}"
   end
+
+	private
+	def avatar_size_validation
+		errors[:avatar] << "should be less than 5.0 MB" if avatar.size > 5.0.megabytes
+	end
 
 end
