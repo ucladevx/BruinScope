875e0871c77071c9c74e5eb3bffdf112e579e3c5
Rohan Kapoor <rohanneilkapoor@gmail.com>
2017-05-31 20:16:48 -0700

    Fixed logic error

--- db/migrate/20170531213408_remove_image_from_users.rb
+++ db/migrate/20170531213408_remove_image_from_users.rb
@@ -1,6 +1,6 @@
 class RemoveImageFromUsers < ActiveRecord::Migration[5.0]
 	def change
-		unless column_exists? :users, :image
+		if column_exists? :users, :image
 			remove_column :users, :image
 		end
 	end
