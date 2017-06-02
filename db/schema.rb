a1e514cdecbfc8687d7305d3e2f8b84ab6cf7daa
Shivam Thapar <shivam.thapar1@gmail.com>
2017-06-02 11:22:26 -0700

    fix overlay closing issue

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20170518130402) do
-=======
 ActiveRecord::Schema.define(version: 20170602033900) do
->>>>>>> master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +50,7 @@ ActiveRecord::Schema.define(version: 20170602033900) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text     "resources"
+    t.integer  "role"
     t.integer  "company_id"
     t.string   "position"
     t.integer  "difficulty"
