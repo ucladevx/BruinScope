1347ed340f0bac13304ccf22fade944942c64c46
Rohan Kapoor <rohanneilkapoor@gmail.com>
2017-06-06 18:34:05 -0700

    search results page wip

--- app/assets/javascripts/carousel.js
+++ app/assets/javascripts/carousel.js
@@ -5,7 +5,7 @@ function initCarousel(className, slides){
 	  infinite: false,
 	  draggable: true,
 	  speed: 400,
-	  slidesToShow: slides,
+	  slidesToShow: 4,
 	  slidesToScroll: slides,
 	  arrows: true,
 	  prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-chevron-left" aria-hidden="true"></i></button>',
@@ -14,8 +14,8 @@ function initCarousel(className, slides){
 	    {
 	      breakpoint: 1024,
 	      settings: {
-	        slidesToShow: 4,
-	        slidesToScroll: 4,
+	        slidesToShow: 10,
+	        slidesToScroll: 10,
 	        infinite: true,
 	        dots: true
 	      }
@@ -23,15 +23,15 @@ function initCarousel(className, slides){
 	    {
 	      breakpoint: 600,
 	      settings: {
-	        slidesToShow: 3,
-	        slidesToScroll: 3
+	        slidesToShow: 10,
+	        slidesToScroll: 10
 	      }
 	    },
 	    {
 	      breakpoint: 480,
 	      settings: {
-	        slidesToShow: 2,
-	        slidesToScroll: 2
+	        slidesToShow: 10,
+	        slidesToScroll: 10
 	      }
 	    }
 	  ]
