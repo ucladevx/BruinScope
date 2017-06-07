b6d22da8ab8d42d945089afd9725156369506609
jcherianucla <jcherian@ucla.edu>
2017-06-07 14:52:33 -0700

    Added S3 region

--- config/initializers/storage.rb
+++ config/initializers/storage.rb
@@ -3,7 +3,8 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     provider:               'AWS',                    
     aws_access_key_id:      ENV["AWS_ACCESS_ID"],    
-    aws_secret_access_key:  ENV["AWS_SECRET_KEY"]
+    aws_secret_access_key:  ENV["AWS_SECRET_KEY"],
+    region:                 'us-east-2'
   }
   config.fog_directory  = ENV["AWS_BUCKET"]
 	config.fog_public = false
