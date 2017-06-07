6b1f55605258c30a2863d453fdbefd9e7e1a92c3
jcherianucla <jcherian@ucla.edu>
2017-06-07 15:58:05 -0700

    Fixing shit

--- app/assets/javascripts/typeahead_search.js
+++ app/assets/javascripts/typeahead_search.js
@@ -1,55 +1,58 @@
 var companies = new Bloodhound({
-  datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
-  queryTokenizer: Bloodhound.tokenizers.whitespace,
-  remote: {
-    url: '/search/typeahead/%QUERY',
-    wildcard: '%QUERY'
-  }
+	datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
+	queryTokenizer: Bloodhound.tokenizers.whitespace,
+	remote: {
+		url: '/search/typeahead/%QUERY',
+		wildcard: '%QUERY'
+	}
 });
 
 function encodeQueryParams(data) {
-   var ret = [];
-   for (var key in data)
-     ret.push(encodeURIComponent(key) + '=' + encodeURIComponent(data[key]));
-   return ret.join('&');
+	var ret = [];
+	for (var key in data)
+		ret.push(encodeURIComponent(key) + '=' + encodeURIComponent(data[key]));
+	return ret.join('&');
 }
 
-$(document).ready(function() {
-  console.log("ayyy");
-  $('#search .typeahead').typeahead({
-    highlight: true
-  },
-  {
-    name: 'companies',
-    display: 'name',
-    source: companies,
-    templates: {
-      suggestion: function(data) {
-        var suggestion = "";
-        switch(data.search_category) {
-          case 'person':
-            suggestion = '<div><a href="/users/' + data.id + '">' + data.name + '</a></div>';
-            break;
-          case 'company':
-            suggestion = '<div><img class="tt-suggestion-img" src="' + data.image + '" /><a href="/companies/' + data.id + '">' + data.name + '</a></div>';
-            break;
-          case 'position':
-            var params = {"exp_position": data.name};
-            suggestion = '<div><a href="/search?' + encodeQueryParams(params) + '">' + data.name + '</a></div>';
-            break;
-          default:
-            break;
-        }
-        return suggestion;
-      }
-    }
-  });
-  $('.navbar-input').on("keypress", function(e) {
-    if (e.keyCode == 13) {
-      console.log(this);
-      var params = {"query": this.value};
-      document.location.href = '/search?' + encodeQueryParams(params);
-      return false; // prevent the button click from happening
-    }
-  });
-})
+function search() {
+	$(document).ready(function() {
+		console.log("ayyy");
+		$('#search .typeahead').typeahead({
+			highlight: true
+		},
+		{
+			name: 'companies',
+			display: 'name',
+			source: companies,
+			templates: {
+				suggestion: function(data) {
+					var suggestion = "";
+					switch(data.search_category) {
+						case 'person':
+							suggestion = '<div><a href="/users/' + data.id + '">' + data.name + '</a></div>';
+							break;
+						case 'company':
+							suggestion = '<div><img class="tt-suggestion-img" src="' + data.image + '" /><a href="/companies/' + data.id + '">' + data.name + '</a></div>';
+							break;
+						case 'position':
+							var params = {"exp_position": data.name};
+							suggestion = '<div><a href="/search?' + encodeQueryParams(params) + '">' + data.name + '</a></div>';
+							break;
+						default:
+							break;
+					}
+					return suggestion;
+				}
+			}
+		});
+		$('.navbar-input').on("keypress", function(e) {
+			if (e.keyCode == 13) {
+				console.log(this);
+				var params = {"query": this.value};
+				document.location.href = '/search?' + encodeQueryParams(params);
+				return false; // prevent the button click from happening
+			}
+		});
+	})
+}
+$(document).on("turbolinks:load", search)
--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -5,8 +5,11 @@ class AvatarUploader < CarrierWave::Uploader::Base
 	include CarrierWave::MiniMagick
 
 	# Choose what kind of storage to use for this uploader:
-	 #storage :file
-	 storage :fog
+	if Rails.env.production?
+		storage :fog
+	else
+		storage :file
+	end
 
 	# Override the directory where uploaded files will be stored.
 	# This is a sensible default for uploaders that are meant to be mounted:
@@ -15,10 +18,10 @@ class AvatarUploader < CarrierWave::Uploader::Base
 	end
 
 	# Provide a default URL as a default if there hasn't been a file uploaded:
-	 def default_url(*args)
-	   # For Rails 3.1+ asset pipeline compatibility:
-	   ActionController::Base.helpers.asset_path("default_avatar")
-	 end
+	def default_url(*args)
+		# For Rails 3.1+ asset pipeline compatibility:
+		ActionController::Base.helpers.asset_path("default_avatar")
+	end
 
 	# Process files as they are uploaded:
 	# process scale: [200, 300]
