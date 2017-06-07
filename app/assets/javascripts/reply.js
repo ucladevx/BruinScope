b4607e4a2d9afd72c0ce36b55466a5915f915623
andyyu <andyhsyu@gmail.com>
2017-06-07 00:47:07 -0700

    delete unnecessary JS, since all comments are in partials in post modal which already contains the eventhandler

--- app/assets/javascripts/reply.js
+++
@@ -1,8 +0,0 @@
-function onVoteHandler(event, data, status, xhr) {
-	var button = $(event.currentTarget);
-	button.find(".upvote-value")[0].innerHTML = "&nbsp+ " + data.upvote;
-	button.toggleClass("upvote-btn-clicked");
-	button.find('#upvote-btn-line').toggleClass("upvote-btn-line-clicked");
-}
-
-$(document).on("ajax:success", "#upvote", onVoteHandler);
